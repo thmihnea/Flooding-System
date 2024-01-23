b7c0ccf796dedbfd2a04659e8f0bc01d850c4c4d
GeorgeMogerTaylor <157011243+GeorgeMogerTaylor@users.noreply.github.com>
2024-01-23 17:43:31 +0000

    Task E with descriptions (and change to flood.py to eliminate the anomalous result)

--- Task2E.py
+++ Task2E.py
@@ -1,4 +1,12 @@
+"""
 
+This file represents a demonstration program for the task 2E.
+We are testing that the function #plot_water_levels
+works properly by showing the first N (in this case, 5) stations
+with the highest relative water level plotted onto graphs showing their 
+water level over the specific time period (in this case 10 days).
+
+"""
 
 
 from floodsystem.flood import stations_highest_rel_level
@@ -10,19 +18,24 @@ import datetime
 
 
 def run() -> None:
-    """
-    This function demonstrates the functionality of this task.
-    """
 
+    #builds list of stations
     stations: list[MonitoringStation] = build_station_list()
 
+    #determines number of graphs generated
     N: int = 5
 
+    #updates water levels
     update_water_levels(stations)
+
+    #sets up time
     dt: int = 10
     dt=datetime.timedelta(days=dt)
 
+    #determines N stations with the highest relative water level
     N_stations = stations_highest_rel_level(stations, N)
+
+    #plots the N stations water level over the given time period
     for station in N_stations:
         dates, levels = fetch_measure_levels(station.measure_id, dt)
         plot_water_levels(station, dates, levels)
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,20 +1,26 @@
-'''
-
-This submodule is designed to focus on plotting using Matplotlib
-
-'''
-
-
 import matplotlib.pyplot as plt
 from floodsystem.station import MonitoringStation
 
+def plot_water_levels(station: list[MonitoringStation], dates: list, levels: list):   
 
+    '''
 
-def plot_water_levels(station, dates, levels):
-    t = dates    
+    Given a MonitoringStation object, return 
+    a MatPlotLib plot showing the variation of the water 
+    level at the station over the given time 
+    period.
+
+    Parameters:
+
+    station: The MonitoringStation object
+    dates: The selection of dates/times from which the data is taken
+    levels: The historical water levels across the time period
+
+    '''
+
     lower_bound = station.typical_range[0]
     upper_bound = station.typical_range[1]
-    plt.plot(t, levels)
+    plt.plot(dates, levels)
     plt.axhline(lower_bound)
     plt.axhline(upper_bound)
     plt.xlabel('date')
